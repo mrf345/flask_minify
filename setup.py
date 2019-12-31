0b36db53476dd38ee5fbe420991c9d350430f4dd
Mohamed Feddad <mrf345@gmail.com>
2019-12-31 19:00:30 +0300

    Update release command. Add `markdown` long description. Bump version 0.16

--- setup.py
+++ setup.py
@@ -7,17 +7,25 @@ javascript, css and less compilation as well.
 
 """
 from setuptools import setup
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='Flask-Minify',
-    version='0.15',
+    version='0.16',
     url='https://github.com/mrf345/flask_minify/',
     download_url='https://github.com/mrf345/flask_minify/archive/0.13.tar.gz',
     license='MIT',
     author='Mohamed Feddad',
     author_email='mrf345@gmail.com',
     description='flask extension to minify html, css, js and less',
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     py_modules=['minify'],
     packages=['flask_minify'],
     zip_safe=False,
