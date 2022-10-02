73f1e4c1f42f30d63fe87888bb9b75a19a3b733b
Mohamed Feddad <mrf345@gmail.com>
2022-10-02 23:59:37 +0400

    refactor: remove the use of `_app_ctx_stack`

---
+++ flask_minify/exceptions.py
@@ -0,0 +1,8 @@
+class FlaskMinifyException(Exception):
+    """FlaskMinify base exception"""
+    pass
+
+
+class MissingApp(FlaskMinifyException):
+    """Raised when the flask app is accessed before it's set"""
+    pass
