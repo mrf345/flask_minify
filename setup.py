b8a40da24f8bb285909fd8ba483e5e7b2a245842
Mohamed Feddad <mrf345@gmail.com>
2023-10-23 02:56:05 +0400

    fix: add go to ci setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 
 from setuptools import setup
 
-supported_versions = ["3.6", "3.7", "3.8", "3.9", "3.10"]
+supported_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
 optional_requirements = {"go": "tdewolff-minify>=2.12.9"}
 basedir = path.abspath(path.dirname(__file__))
 long_description = ""
