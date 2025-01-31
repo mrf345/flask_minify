618ba8975af545df51c3ca17ffe50d3789bd908e
Mohamed Feddad <mrf345@gmail.com>
2025-01-31 04:44:16 +0400

    chore: drop support for python 3.7 and add 3.13

--- flask_minify/about.py
+++ flask_minify/about.py
@@ -3,3 +3,4 @@ __doc__ = "Flask extension to minify html, css, js and less."
 __license__ = "MIT"
 __author__ = "Mohamed Feddad"
 __email__ = "mrf345@gmail.com"
+__supported_versions__ = ["3.8", "3.9", "3.10", "3.11", "3.12", "3.13"]
